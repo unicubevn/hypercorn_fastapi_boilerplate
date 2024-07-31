b43c871483e49d66f006d2edeea9e4174830952b
Jean Nguyen <anhjean@gmail.com>
2024-07-31 23:30:53 +0700

    Update cubelogger.py

--- server/cubelogger.py
+++ server/cubelogger.py
@@ -7,14 +7,16 @@ class CubeFormatter(logging.Formatter):
     grey = "\x1b[38;20m"
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
+    blue = "\x1b[34;20m"
+    cyan = "\x1b[36;20m"
     bold_red = "\x1b[31;1m"
     green = '\x1b[32m'
     reset = "\x1b[0m"
     format = """[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)d] "%(message)s" """
 
     FORMATS = {
-        logging.DEBUG: green + format + reset,
-        logging.INFO: grey + format + reset,
+        logging.DEBUG: blue + format + reset,
+        logging.INFO: green + format + reset,
         logging.WARNING: yellow + format + reset,
         logging.ERROR: red + format + reset,
         logging.CRITICAL: bold_red + format + reset
